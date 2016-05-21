d4f3658dbdc888bc1e443aecf26b51a9a2330035
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-21 17:03:06 -0600

    create class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,10 +1,17 @@
 require_relative 'content_parser'
 require_relative "braille_parser"
+require_relative "braille_decryptor"
 require 'pry'
 
     contents = File.read(ARGV[0])
 
     content_parser = ContentParser.new(contents)
-    parsed_contents = content_parser.parse_file.map{|line| line.flatten}
+    decryptor = BrailleDecryptor.new
+    
+    content_parser.parse_file
+    content_parser.format_content
+    formatted_braille = content_parser.sorted_braille
     # binding.pry
-    File.write(ARGV[1], parsed_contents)
+    decrypted_braille = formatted_braille.map {|letter| decryptor.decipher[letter]}.join
+
+    File.write(ARGV[1], decrypted_braille)

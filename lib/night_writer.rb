80cce62c5a1b93d31242f7bc08f5c43b844dba47
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-21 14:02:34 -0600

    find way to add array elements with matching indices

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,10 @@
-contents = File.read(ARGV[0])
+require_relative 'content_parser'
+require_relative "braille_parser"
+require 'pry'
 
+    contents = File.read(ARGV[0])
 
-
-File.write(ARGV[1], contents)
+    content_parser = ContentParser.new(contents)
+    parsed_contents = content_parser.parse_file.map{|line| line.flatten}
+    # binding.pry
+    File.write(ARGV[1], parsed_contents)

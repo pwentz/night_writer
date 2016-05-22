259a8b857024812dca866a2cd8ec28742c1e87f8
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-22 16:24:03 -0600

    still requires testing

---
+++ lib/encryption_formatter.rb
@@ -0,0 +1,18 @@
+require_relative 'encryptor'
+require 'pry'
+
+class EncryptionFormatter
+  attr_reader :final_encryption
+  def initialize(encrypted_text)
+    @encrypted_text = encrypted_text
+  end
+
+  def format
+    formatted_encryption = Array.new
+    until @encrypted_text.all?{|nests|nests.empty?}
+      formatted_encryption.push(@encrypted_text.map{|nest|nest[0+0+0]})
+      @encrypted_text.each{|nest|nest.shift}
+    end
+    @final_encryption = formatted_encryption.map{|line|line.push("\n")}.join
+  end
+end

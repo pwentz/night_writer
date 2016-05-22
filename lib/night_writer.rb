066dc89cc1ca7a5432f28083d0378d0fe24a684c
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-22 16:24:43 -0600

    fully functioning, consider refactoring

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,17 +1,39 @@
 require_relative 'content_parser'
 require_relative "braille_parser"
 require_relative "braille_decryptor"
-require 'pry'
+require_relative "encryption_formatter"
 
+require 'pry'
+class NightWriter
+  attr_reader :decrypted_braille
+  def initialize
+    @decrypted_braille
+  end
+  def decrypt
     contents = File.read(ARGV[0])
 
     content_parser = ContentParser.new(contents)
     decryptor = BrailleDecryptor.new
-    
+
     content_parser.parse_file
     content_parser.format_content
-    formatted_braille = content_parser.sorted_braille
-    # binding.pry
-    decrypted_braille = formatted_braille.map {|letter| decryptor.decipher[letter]}.join
+    File.write(ARGV[1], content_parser.decrypted_braille)
+  end
 
-    File.write(ARGV[1], decrypted_braille)
+  def encrypt
+    plain_text = File.read(ARGV[0])
+    encryptor = Encryptor.new(plain_text)
+    encryptor.parse
+    formatter = EncryptionFormatter.new(encryptor.encrypted_text)
+    formatter.format
+    File.write(ARGV[1],formatter.final_encryption)
+  end
+
+  night_writer = NightWriter.new
+  if File.readlines(ARGV[0]).count == 3
+    night_writer.decrypt
+  else
+    night_writer.encrypt
+  end
+
+end
